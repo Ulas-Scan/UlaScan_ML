4ee70d6c2c9bdf9dbe558c16a1aa3c1fde9c831e
iyoubee <al.ayubi2020@gmail.com>
2024-06-09 06:10:33 +0700

    change to method post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from utils import get_model, get_tokenizer, predict_sentiment
 
 app = Flask(__name__)
@@ -10,15 +10,22 @@ PRETRAINED_PATH = 'transformers-bert'
 tokenizer = get_tokenizer(MODEL_NAME)
 model = get_model(PRETRAINED_PATH)
 
-@app.route("/predict/", methods=['GET'])
+@app.route("/predict/", methods=['POST'])
 def predict():
-    statements = request.json.get("statements")
+    data = request.json
+    if data is None:
+        return jsonify({"error": "Invalid JSON"}), 400
+    
+    statements = data.get("statements")
+    if statements is None:
+        return jsonify({"error": "No statements provided"}), 400
+    
     # statements: list of reviews
     report = {'Positive': 0, 'Negative': 0}
     for statement in statements:
         sentiment = predict_sentiment(statement, tokenizer, model, MAX_LENGTH)
         report[sentiment] += 1
-    return report
+    return jsonify(report)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
